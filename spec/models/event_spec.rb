c4eef7487f7d288e77d1195d9c44e0b404a979e5
jobvanduuren <job_van_duuren@hotmail.com>
2017-05-24 16:23:18 +0200

    Add model specs: associations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -25,4 +25,30 @@ RSpec.describe Event, type: :model do
       expect(Event.order_by_price).to match_array [event1, event2, event3]
     end
   end
+
+  describe "association with user" do
+  let(:user) { create :user }
+
+  it "belongs to a user" do
+    event = user.events.new(name: "Shared")
+
+    expect(event.user).to eq(user)
+  end
+end
+
+describe "association with theme" do
+  let(:event) { create :event }
+
+  let(:theme1) { create :theme, name: "Bright", events: [event] }
+  let(:theme2) { create :theme, name: "Clean lines", events: [event] }
+  let(:theme3) { create :theme, name: "A Man's Touch", events: [event] }
+
+  it "has themes" do
+    expect(event.themes).to include(theme1)
+    expect(event.themes).to include(theme2)
+    expect(event.themes).to include(theme3)
+  end
+end
+
+
 end
