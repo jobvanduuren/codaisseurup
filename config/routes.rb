eb1af0b205feb118c5416e31867c32b91f51b477
jobvanduuren <job_van_duuren@hotmail.com>
2017-05-25 18:12:15 +0200

    Add about page test

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users
-  resources :profiles, only: [:new, :edit, :create, :update] 
+  resources :profiles, only: [:new, :edit, :create, :update]
   resources :users, only: [:show]
   resources :events
+  get "about" => "pages#about"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
